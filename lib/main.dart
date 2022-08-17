1f846815e73ec9388125b3308f896725561fbba3
FlutLab.io <>
2022-08-17 17:15:08 +0000

    premiers modif

--- lib/main.dart
+++ lib/main.dart
@@ -34,60 +34,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: const Text(
-          "BackdropFilter",
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.black,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          title: const Text(
+            "BackdropFilter",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.black,
+            ),
           ),
+          actions: [
+            const Icon(Icons.computer_sharp, color: Colors.black, size: 30),
+          ],
         ),
-        actions: [
-          const Icon(Icons.computer_sharp, color: Colors.black, size: 30),
-        ],
-      ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-              shape: BoxShape.rectangle,
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://cdn.futura-sciences.com/buildsv6/images/wide1920/8/8/6/886a1f5f0b_84113_fs-01-13668997695-1b11821209-k.jpg"),
-                fit: BoxFit.fill,
-              ),
-            ),
+        body: Center(
+          child: Column(
+            children: [
+              Text("test"),
+            ],
           ),
-          Positioned(
-            top: 100,
-            right: 50,
-            left: 50,
-            bottom: 100,
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  color: Colors.white.withOpacity(0.5),
-                  child: Column(
-                    children: [
-                      const Text(
-                        "Bienvenue chez Momodev",
-                        style: TextStyle(fontSize: 30, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
